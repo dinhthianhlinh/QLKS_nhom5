76c45bb53a8e9f37f1705459613447b8811eec38
ADMIN <hocn34878@gmail.com>
2023-11-25 22:29:25 +0700

    học up

---
+++ app/src/main/java/com/example/quanlikhachsan_nhom5/ManHinhKhachHang/TrangChu.java
@@ -0,0 +1,18 @@
+package com.example.quanlikhachsan_nhom5.ManHinhKhachHang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quanlikhachsan_nhom5.R;
+
+public class TrangChu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trang_chu);
+
+        //Học
+    }
+}
